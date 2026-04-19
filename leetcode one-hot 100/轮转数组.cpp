e805e387d499c067cc95010ec2d5e8036b555437
Tengyue Jiang <19822763301@163.com>
2026-04-19 19:56:18 +0800

    Implement rotate function for array rotation

--- leetcode one-hot 100/轮转数组.cpp
+++ leetcode one-hot 100/轮转数组.cpp
@@ -15,3 +15,17 @@ public:
         }
     }
 };
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int s=nums.size();
+        vector<int> p(nums.size());
+        for(int i=0;i<nums.size();i++){
+            p[(i+k)%s]=nums[i];
+        }
+        for(int i=0;i<nums.size();i++)
+        nums[i]=p[i];
+    }    
+};
